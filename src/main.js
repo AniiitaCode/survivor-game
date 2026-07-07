841e416d17e8564d6e514e878788b51480ccc642
Aniiita <anita_nen4eva@abv.bg>
2026-07-07 16:37:26 +0300

    add main and styles

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './styles.css';
+import { initRouter } from './router.js';
+
+initRouter();
